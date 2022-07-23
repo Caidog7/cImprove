24ae0cf009d3f65c812efc4f370ed8090c3b5cc4
caidog7 <1346709873@qq.com>
2022-07-23 15:42:52 +0800

    添加指针数组和数组指针文件

---
+++ day01/06_数组指针和指针数组.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+     
+   
+    const char* a[] = { "aaa","bbb","ccc" }; //指针数组是数组
+    char b[10] = { "abc" };
+    char(*p)[10];//数组指针是指针，不常用
+    p = &b;
+
+    printf("a:0x%x,*a:%s\n",(int)a[0], a[0]);
+    printf("q:0x%x,*q:%c,q:0x%x\n", (int)b, b[0], &b[0]);
+    printf("q:0x%x,*q:%c,q:0x%x\n", (int)p, p[0], &p[0]);//数组指针输出时需要带(*p)[0]，不能直接p[0]
+
+
+    typedef int(*P)[10];//数组指针类型
+    P q;//数组指针变量
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
